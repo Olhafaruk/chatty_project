8a7b66d7913436e59380863dde541ba6ab33d470
anatoliy-zhuk <af3557771@gmail.com>
2025-05-07 22:14:37 +0300

    рефакторинг 3 добавлен docker-compose.yml и другие файлы dockers

--- posts/views.py
+++ posts/views.py
@@ -1,3 +1,28 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.views.generic import UpdateView, DeleteView
+from django.urls import reverse_lazy
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from .models import Post
+from .forms import PostForm  # если форма уже есть
+
+class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
+    model = Post
+    form_class = PostForm
+    template_name = 'posts/post_form.html'  # переиспользуй шаблон создания
+    success_url = reverse_lazy('home')  # или куда тебе нужно
+
+    def test_func(self):
+        post = self.get_object()
+        return self.request.user == post.author
+
+
+class PostDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
+    model = Post
+    template_name = 'posts/post_confirm_delete.html'
+    success_url = reverse_lazy('home')
+
+    def test_func(self):
+        post = self.get_object()
+        return self.request.user == post.author
