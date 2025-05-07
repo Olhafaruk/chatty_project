81115b56284f58993f084a3f7ead5601ea3c124f
Sergei Lisitsyn <autoanastasia@narod.ru>
2025-05-07 19:29:01 +0200

    переписаны VIEWs как классовые

--- posts/urls.py
+++ posts/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('posts/', views.post_list, name='post_list'),
+    path('posts/', views.PostListView.as_view(), name='post_list'),
+    path('posts/<int:pk>/', views.PostDetailView.as_view(), name='post_detail_by_id'),
     path('posts/<int:post_id>/', views.post_detail, name='post_detail'),
 ]
