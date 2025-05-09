4399b6026163c85b92d2a95ec027ad046e8cd2d6
anatoliy-zhuk <af3557771@gmail.com>
2025-05-09 19:37:11 +0300

    Рефакторинг страницы home.html и создание login.html

--- posts/urls.py
+++ posts/urls.py
@@ -2,9 +2,11 @@ from django.urls import path
 from . import views
 from .views import PostUpdateView, PostDeleteView, PostCreateView, PostDetailView
 from django.views.generic import TemplateView
+from django.contrib.auth.views import LoginView
 
 urlpatterns = [
     path('home/', TemplateView.as_view(template_name='home.html'), name='home'),
+    path('login/', LoginView.as_view(template_name='login.html'), name='login'),
     path('', views.PostListView.as_view(), name='post_list'),                        # Список постов
     path('create/', PostCreateView.as_view(), name='post_create'),                   # Создание поста
     path('<slug:slug>/edit/', PostUpdateView.as_view(), name='post_edit'),           # Редактирование
