c14f7011b856c8699bc2bc2a22cabb6032320a2b
Olha Faruk <farukolga2017@gmail.com>
2025-05-10 15:45:42 +0200

    рефакторинг post_detail.htm, едирект после входа — теперь после авторизации пользователь попадает на /posts/, а не /profile/

--- users/urls.py
+++ users/urls.py
@@ -2,13 +2,16 @@ from django.urls import path
 from django.contrib.auth import views as auth_views
 from . import views
 from .views import CustomLoginView
+from django.urls import reverse_lazy
 
 urlpatterns = [
     path('', views.welcome_view, name='welcome'),  # Приветственная страница
     path('home/', views.home_view, name='home'),  # Основная страница
     path('register/', views.register, name='register'),
     path('login/', CustomLoginView.as_view(template_name='users/login.html'), name='login'),  # используем кастомное представление
-    path('logout/', auth_views.LogoutView.as_view(next_page='home'), name='logout'),
+    #path('logout/', auth_views.LogoutView.as_view(next_page='home'), name='logout'),
+    path('logout/', auth_views.LogoutView.as_view(next_page=reverse_lazy('post_list')), name='logout'),
+
 
     path('profile/<str:username>/', views.profile, name='profile'),
     path('change_password/', views.change_password, name='change_password'),
