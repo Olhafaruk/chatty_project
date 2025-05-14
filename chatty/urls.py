d2417d5b2a44166e16300ba561bb2837f73a6e66
Olha Faruk <farukolga2017@gmail.com>
2025-05-14 19:16:24 +0200

    +subscriptions

--- chatty/urls.py
+++ chatty/urls.py
@@ -11,9 +11,9 @@ urlpatterns = [
     path('accounts/', include('django.contrib.auth.urls')),
     path('', include('users.urls')),
     path('posts/', include('posts.urls', namespace='posts')),
-
-    path('posts/', include('posts.urls')),# Включаем URL-адреса приложения posts
-    path('subscriptions/', include('subscriptions.urls')),# Включаем URL-адреса приложения subscriptions
+    path('', include('users.urls')),
+    #path('posts/', include('posts.urls')),# Включаем URL-адреса приложения posts
+    path('subscriptions/', include('subscriptions.urls', namespace='subscriptions')),# Включаем URL-адреса приложения subscriptions
 ]
 
 # Добавляем поддержку медиа-файлов (аватары, изображения и т.д.)
