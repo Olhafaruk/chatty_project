7d32f2b0b285b84fa97fdca6729b8b8781d406dc
Sergei Lisitsyn <autoanastasia@narod.ru>
2025-05-15 19:03:25 +0200

    tests is go

--- chatty/settings.py
+++ chatty/settings.py
@@ -107,13 +107,13 @@ WSGI_APPLICATION = 'chatty.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
-"""DATABASES = {
+DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
-}"""
-
+}
+"""
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
@@ -123,7 +123,7 @@ DATABASES = {
         'HOST': os.getenv('PG_HOST'),
         'PORT': os.getenv('PG_PORT'),
     }
-}
+}"""
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
